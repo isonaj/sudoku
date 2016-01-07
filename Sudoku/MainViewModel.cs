214cdbe6e1d9a18ab4376dc4bb462ffdbba77fa6
unknown <anthony@AnthonyIson.ssa.local>
2016-01-07 13:19:23 +1000

    Initial layout

---
+++ Sudoku/MainViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sudoku
+{
+    public class MainViewModel
+    {
+        public List<List<int>> Values { get; set; }
+
+        public MainViewModel()
+        {
+            Values = new List<List<int>> {
+                new List<int> { 1, 3, 4, 5, 2, 8, 7, 6, 9 },
+                new List<int> { 1, 3, 4, 5, 2, 8, 7, 6, 9 },
+                new List<int> { 1, 3, 4, 5, 2, 8, 7, 6, 9 },
+                new List<int> { 1, 3, 4, 5, 2, 8, 7, 6, 9 },
+                new List<int> { 1, 3, 4, 5, 2, 8, 7, 6, 9 },
+                new List<int> { 1, 3, 4, 5, 2, 8, 7, 6, 9 },
+                new List<int> { 1, 3, 4, 5, 2, 8, 7, 6, 9 },
+                new List<int> { 1, 3, 4, 5, 2, 8, 7, 6, 9 },
+                new List<int> { 1, 3, 4, 5, 2, 8, 7, 6, 9 }
+            };
+        }
+    }
+}
